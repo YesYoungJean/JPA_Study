18efc947f73bd75cada3151000919b4c6d7af100
develop-yjnoh <114342669+develop-yjnoh@users.noreply.github.com>
2023-07-26 15:37:58 +0900

    Lombok 적용

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,10 +1,15 @@
 package hellojpa;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 //jpa 사용한다고 인식
 @Entity
+@Getter
+@Setter
 public class Member {
 
     //PK 명시
@@ -12,19 +17,19 @@ public class Member {
     private Long id;
     private String name;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
 }
